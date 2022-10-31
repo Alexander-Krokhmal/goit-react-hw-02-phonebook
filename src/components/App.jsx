64a37243877debfc82d1fddf13ef8e3ec9e75559
Alexander-Krokhmal <zeddicus.ak@gmail.com>
2022-10-31 15:21:14 +0200

    Add last task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,53 @@
 import React, { Component } from 'react';
-import { nanoid } from 'nanoid';
 import { Section } from './Section/Section';
+import ContactForm from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import Filter from './Filter/Filter';
 
 class App extends Component {
   state = {
-    contacts: [],
-    name: '',
-    number: '',
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
   };
 
-  nameInputId = nanoid();
-  telInputId = nanoid();
-
-  handleChange = e => {
-    console.log(e.target);
-    this.setState({ [e.target.name]: e.target.value });
+  addContact = (name, number, id) => {
+    this.setState(prevState => {
+      return {
+        contacts: [
+          ...prevState.contacts,
+          { name: name, number: number, id: id },
+        ],
+      };
+    });
   };
 
-  handleSubmit = e => {
-    const name = this.state;
-    // const number = this.state;
-    e.preventDefault();
-    // console.log('this.state :>> ', this.state);
-    this.setState(prevState => {return { contacts: [...prevState.contacts, name] }});
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
   };
 
-  // handleSubmit = (name, number) => {
-  //   this.setState(prevState => {return { contacts: [...prevState.contacts, {name: name, number: number}] }});
-  // };
+  filterContacts = e => {
+    this.setState({ filter: e.target.value });
+
+  };
+
+  renderContacts = () => {
+    const normalizedFilter = this.state.filter.toLowerCase();
+    
+    const visibleContact = this.state.contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter));
+
+    return visibleContact;
+  }
 
   render() {
     // const name = this.state;
+
 
     return (
       <div
@@ -45,47 +62,22 @@ class App extends Component {
         }}
       >
         <Section title="Phonebook">
-          <form onSubmit={this.handleSubmit}>
-            <label htmlFor={this.nameInputId}>Name </label>
-            <input
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-              id={this.nameInputId}
-              value={this.state.name}
-              placeholder="Alex Krom"
-              onChange={this.handleChange}
-            />
-
-            <label htmlFor={this.telInputId}>Number </label>
-            <input
-              type="tel"
-              name="number"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-              id={this.telInputId}
-              value={this.state.number}
-              placeholder="227-91-26"
-              onChange={this.handleChange}
-            />
-
-            <button type="submit">Add contact</button>
-          </form>
+          <ContactForm
+            items={this.state.contacts}
+            addContactsProps={this.addContact}
+          ></ContactForm>
         </Section>
 
         <Section title="Contacts">
-          <ul>
-            {this.state.contacts.map(contact => (
-              <li key={contact.name}>
-                <span> {contact.name}: {contact.number}</span>
-              </li>
-            )
-            )}
-            
-          </ul>
+          <Filter
+            value={this.state.filter}
+            onChangeProps={this.filterContacts}>
+          </Filter>
+
+          <ContactList
+            items={this.renderContacts()}
+            deleteContactProps={this.deleteContact}
+          ></ContactList>
         </Section>
       </div>
     );
